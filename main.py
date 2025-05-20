fe8ea115057f85d9581abbbdb85691754d87b1e0
habibasoliman <Cds.habibamohamed71311@alexu.edu.eg>
2025-05-21 01:30:50 +0300

    fastpai endpoints

---
+++ main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import mlflow.pyfunc
+import joblib
+
+
+app = FastAPI(title="Bank Customer Churn Prediction API")
+
+#model_name = "bank_churn_model"  
+#model_stage = "Production"
+
+#model = mlflow.pyfunc.load_model(model_uri=f"models:/{model_name}/{model_stage}")
+
+# Define the input schema
+model = joblib.load("model.pkl")
+
+class CustomerData(BaseModel):
+    CreditScore: int
+    Geography: str
+    Gender: str
+    Age: int
+    Tenure: int
+    Balance: float
+    NumOfProducts: int
+    HasCrCard: int
+    IsActiveMember: int
+    EstimatedSalary: float
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Churn Prediction API"}
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
+
+@app.post("/predict")
+def predict(data: CustomerData):
+    input_df = data.dict()
+    input_df = [input_df]  
+    prediction = model.predict(input_df)
+    return {"prediction": int(prediction[0])}
